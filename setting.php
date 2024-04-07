a05763acd2357a39e65f626afe5af376b8b7ebe3
cuongnguyen0910 <165142635+cuongnguyen0910@users.noreply.github.com>
2024-04-07 12:38:27 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s104997407"; // your user name
+	$pwd = "010105"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s104997407_db"; // your database
+?>
